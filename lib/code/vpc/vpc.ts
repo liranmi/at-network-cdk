d35a38582373da3de65a91038bdb68ec15d4eda3
Liran Mishali <liranmi@gmail.com>
2025-05-28 16:30:43 +0300

    Add exports to vpc stack

--- lib/code/vpc/vpc.ts
+++ lib/code/vpc/vpc.ts
@@ -2,6 +2,7 @@ import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Tags } from 'aws-cdk-lib';
 import { VpcConfig } from '../../schemas/vpc';
+import * as cdk from 'aws-cdk-lib';
 
 export interface CustomVpcProps {
     vpcConfig: VpcConfig;
@@ -16,15 +17,30 @@ export class CustomVpc extends Construct {
         const { subnetConfigs, version, tags, maxAzs = 1, ...vpcProps } = props.vpcConfig;
 
         // Prevent auto subnet creation by passing an empty subnetConfiguration and set maxAzs to 1
-        this.vpc = new ec2.Vpc(this, 'Resource', {
+        const vpc = new ec2.Vpc(this, 'Resource', {
             ...vpcProps,
             subnetConfiguration: [],
             maxAzs,
         });
 
+        // Export the VPC ID
+        new cdk.CfnOutput(this, 'VpcId', {
+            value: vpc.vpcId,
+            description: 'The ID of the VPC',
+            exportName: `${id}-vpc-id`
+        });
+
+
+        // Create subnets
         if (subnetConfigs) {
             for (const subnetConfig of subnetConfigs) {
-                new ec2.Subnet(this, subnetConfig.name, subnetConfig);
+                const subnet = new ec2.Subnet(this, subnetConfig.name, subnetConfig);
+                // Export the subnet ID
+                new cdk.CfnOutput(this, `${subnetConfig.name}-SubnetId`, {
+                    value: subnet.subnetId,
+                    description: `The ID of the ${subnetConfig.name} subnet`,
+                    exportName: `${id}-${subnetConfig.name}-subnet-id`
+                });
             }
         }
 
